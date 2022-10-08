17ffb75942aae1038a3574fd39a3ef76708061f8
jmh <jiamenghu123@163.com>
2022-10-08 19:17:13 +0800

    add Script Files

---
+++ Script/Run.sh
@@ -0,0 +1,4 @@
+(sh Run_BasketballPass_416x240_50.sh &);
+(sh Run_BlowingBubbles_416x240_50.sh &);
+(sh Run_BQSquare_416x240_60.sh &);
+(sh Run_RaceHorses_416x240_30.sh &);
---
+++ Script/RunMe.bat
@@ -0,0 +1,22 @@
+@echo off
+
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 1
+ping 127.1 -n 2 >nul
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 2
+ping 127.1 -n 2 >nul
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 3
+ping 127.1 -n 2 >nul
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 4
+ping 127.1 -n 2 >nul
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 5
+ping 127.1 -n 2 >nul
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 6
+ping 127.1 -n 2 >nul
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 7
+ping 127.1 -n 2 >nul
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 8
+ping 127.1 -n 2 >nul
+start VTM_LB10_x_encoderAppVTM201_decoderAppVTM201_x.bat 9
+ping 127.1 -n 2 >nul
+
+exit
